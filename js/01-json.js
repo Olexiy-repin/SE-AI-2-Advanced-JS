64d31c2e8c7c8aaf3aa25acd9b9ddc1d464c3751
Olexiy-repin <repinlyoha@gmail.com>
2026-09-21 21:35:29 +0300

    classwork

---
+++ js/01-json.js
@@ -0,0 +1,58 @@
+/*
+ * Формат JSON
+ *
+ * - Метод JSON.stringify(value)
+ * - Метод JSON.parse(value)
+ * - Перетворення функцій
+ */
+// const user = {
+//   firstName: 'Oleksii',
+//   lastName: 'Repin',
+//   age: 30,
+
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+// const userJSON = JSON.stringify(user);
+
+// console.log(user);
+// console.log(userJSON);
+
+// const users = [
+//   {
+//     firstName: 'Garrett',
+//     lastName: 'Greene',
+//     age: 30,
+//   },
+//   {
+//     firstName: 'Randall',
+//     lastName: 'Jackson',
+//     age: 20,
+//   },
+//   {
+//     firstName: 'Randy',
+//     lastName: 'Lamb',
+//     age: 40,
+//   },
+// ];
+
+// const usersJSON = JSON.stringify(users);
+
+// console.log(users);
+// console.log(usersJSON);
+
+/*
+ * Конструкція try...catch для обробки помилок
+ */
+// console.log('Start');
+
+// try {
+//   let firstName = 'Oleksii';
+
+//   firstName = 'Max';
+// } catch (err) {
+//   console.log(err);
+// }
+
+// console.log('End');
---
+++ js/02-localstorage.js
@@ -0,0 +1,36 @@
+/*
+ * LocalStorage
+ */
+
+// console.log(localStorage);
+// console.log(sessionStorage);
+
+/*
+ * Збереження
+ * Чому треба використовувати метод JSON.stringify
+ */
+
+// localStorage.setItem('site-theme', 'dark');
+
+// const product = {
+//   title: 'Apple',
+//   price: 80,
+//   quantity: 2,
+// };
+
+// localStorage.setItem('product-info', JSON.stringify(product));
+
+/*
+ * Читання
+ * Чому треба використовувати метод JSON.parse
+ */
+
+// console.log(localStorage.getItem('site-theme'));
+// console.log(JSON.parse(localStorage.getItem('product-info')));
+
+/*
+ * Видалення
+ */
+
+// localStorage.clear();
+// localStorage.removeItem('product-info');
